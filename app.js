32720defff65894255b15144ba926bc9d12def01
moctar <mjob274@gmail.com>
2021-05-18 23:17:43 +0200

    installation de body-parser, ajout post de nouvel objet, ajout middleware de parse json de la requete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -27,6 +28,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
+});
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
